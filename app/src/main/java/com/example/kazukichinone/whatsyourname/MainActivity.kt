6e3103a7b3abc7bcd475abdd458af1c09a1953f2
Kazuki Chinone <staybesidehangon@gmail.com>
2019-09-04 19:55:37 +0900

    second commit

--- app/src/main/java/com/example/kazukichinone/whatsyourname/MainActivity.kt
+++ app/src/main/java/com/example/kazukichinone/whatsyourname/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.kazukichinone.whatsyourname
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = editText.text.toString() + "さん、こんにちは"
+            imageView.setImageResource(R.drawable.icon)
+        }
     }
 }
